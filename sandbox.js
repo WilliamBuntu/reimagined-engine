a82efb52393eac32ca688e8312d53e88fb2d6286
Buntu William <williambuntu1@gmail.com>
2023-04-25 20:53:52 +0200

    first commit

---
+++ sandbox.js
@@ -0,0 +1,10 @@
+const buttons = document.querySelectorAll('.buttons .btn');
+const notf  = document.querySelector('.notifications');
+
+buttons.forEach(btn => {
+    btn.addEventListener('click',()=>{
+        //console.log('abusddd')
+        
+
+    })
+})
